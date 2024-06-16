7f3e967cc6e1dfca2bc519a495de3f0d9d4b768b
MADEWGN-X <github@madewgn.eu.org>
2024-06-16 23:06:55 +0000

    4ewff

--- p.py
+++ p.py
@@ -1,4 +1,5 @@
 import os
 
-for i in range(50):
-    os.system(f"screen -dmS {i}A bash -c 'python3 on.py'")
+for i in range(200):
+    print(i)
+    os.system(f"screen -dmS {i}b bash -c 'python3 on.py'")
